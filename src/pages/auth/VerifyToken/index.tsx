597ab1adc77c62b52cde1e9a99d23aa43d0fb9e2
Dumildes Paulo <dumildesfernando@outlook.com>
2024-05-03 22:40:22 +0100

    refactor: remove unused imports

--- src/pages/auth/VerifyToken/index.tsx
+++ src/pages/auth/VerifyToken/index.tsx
@@ -4,7 +4,7 @@ import Button, { ButtonColors } from "../../../components/forms/Button";
 import FormContainer from "../../../components/forms/FormContainer";
 import Input from "../../../components/forms/Input";
 import Main from "../../../layouts/Main";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 
 export default function VerifyToken() {
   const [params] = useSearchParams();
